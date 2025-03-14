60f64bd99832cd6d564810e00e676a9ca244e59b
P1nhero <p1nhero2005@gmail.com>
2025-03-14 15:31:30 -0700

    Add files via upload

---
+++ atividade/Atividade.java
@@ -0,0 +1,17 @@
+package atividade;
+
+import java.util.Scanner;
+
+public class Atividade {
+
+	public static void main(String[] args) {
+		double n1;
+		
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.println("digite sua idade");
+		n1 =  ler.nextDouble();
+
+	}
+
+}
